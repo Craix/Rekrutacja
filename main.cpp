64792f323a3943c2da30a29ee85f81a8e0288cd3
Craix <maciekszostak124@gmail.com>
2021-05-18 18:49:36 +0200

    Zadanie 1

--- main.cpp
+++ main.cpp
@@ -1,71 +1,139 @@
 #include <iostream>
-#include <string>
-#include <unordered_map>
-#include <random>
+#include <type_traits>
 
-#include <chrono>
-using namespace std::chrono;
+struct vec3 {
+    float x, y, z;
 
-#define SIZE 100
+    float magnitude() {
+        return sqrt(x * x + y * y + z * z);
+    }
 
-int main()
+    vec3(const vec3& vec) {
+        this->x = vec.x;
+        this->y = vec.y;
+        this->z = vec.z;
+    }
+    vec3(float x, float y, float z) {
+        this->x = x;
+        this->y = y;
+        this->z = z;
+    }
+
+    vec3 operator / (float num) {
+        return vec3(x / num, y / num, z / num);
+    }
+
+    vec3 operator * (float num) {
+        return vec3(x * num, y * num, z * num);
+    }
+
+    vec3 operator * (const vec3& vec) {
+        return vec3(x * vec.x, y * vec.y, z * vec.z);
+    }
+
+
+    vec3 operator + (const vec3& vec) {
+        return vec3(x + vec.x, y + vec.y, z + vec.z);
+    }
+
+    vec3 operator - (const vec3& vec) {
+        return vec3(x - vec.x, y - vec.y, z - vec.z);
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const vec3& vec);
+
+};
+
+std::ostream& operator<<(std::ostream& os, const vec3& vec) {
+    os << "[" << vec.x << ", " << vec.y << ", " << vec.z << "]";
+    return os;
+}
+
+vec3 abs(vec3 vec)
 {
-	//unordered_set / unordered_map -> O(1) (avarage) or O(n) (worst)
-	std::unordered_map <std::string, int> map;
+    return vec3(abs(vec.x), abs(vec.y), abs(vec.z));
+}
 
-	//fil random 
-	for (int i = 0; i < SIZE; i++) {
-		map[std::to_string(i)] = i;
-	}
+// Implement this function for the following shapes:
+struct Sphere {
+    vec3 center;
+    float radius;
+};
 
-	srand(static_cast<long int> (time(NULL)));
-	int j = 0, tmp = 0;
+struct Aabb {
+    vec3 min;
+    vec3 max;
+};
 
-	for (int i = 0; i < SIZE; i++) {
-		j = rand() % 100;
-		tmp = map[std::to_string(i)];
-		map[std::to_string(i)] = map[std::to_string(j)];
-		map[std::to_string(j)] = tmp;
-	}
+// Triangular pyramid.
+struct Tetrahedron
+{
+    vec3 points[4];
+};
 
-	std::cout << "Container size: " << map.size() << std::endl;
+// Cylinder with hemispherical ends.
+struct Capsule
+{
+    vec3 points[2];
+    float radius;
+};
 
-	for (int i = 0; i < (map.size() - 1); i++) {
-		std::cout << map[std::to_string(i)] << ", ";
-	}
-	std::cout << map[std::to_string(map.size() - 1)] << std::endl;
+template <typename Shape> vec3 support(vec3 v, Shape  s) {
 
-	//delete first element
-	auto start = high_resolution_clock::now();
-	map.erase(std::to_string(0));
-	auto stop = high_resolution_clock::now();
-	std::cout << "Time: " << duration_cast<microseconds>(stop - start).count() << std::endl;
+    vec3 dir(v / v.magnitude());
 
-	//delete laast element
-	start = high_resolution_clock::now();
-	map.erase(std::to_string(map.size() - 1));
-	stop = high_resolution_clock::now();
-	std::cout << "Time: " << duration_cast<microseconds>(stop - start).count() << std::endl;
+    if constexpr (std::is_same<Shape, Sphere>::value) {
 
-	//delete random element
-	start = high_resolution_clock::now();
-	map.erase(std::to_string(rand() % 100));
-	stop = high_resolution_clock::now();
-	std::cout << "Time: " << duration_cast<microseconds>(stop - start).count() << std::endl;
+        return (s.center + (dir * s.radius));
+    }
 
-	//delete 10 random element
-	for (int i = 0; i < 10; i++) {
-		start = high_resolution_clock::now();
-		map.erase(std::to_string(rand() % 100));
-		stop = high_resolution_clock::now();
-		std::cout << "Time: " << duration_cast<microseconds>(stop - start).count() << std::endl;
-	}
+    if constexpr (std::is_same<Shape, Aabb>::value) {
+        vec3 center(((s.min + s.max) / 2));
+        vec3 size2(abs(s.min - s.max) / 2);
 
-	//delete random element
-	start = high_resolution_clock::now();
-	map.erase(std::to_string(123));
-	stop = high_resolution_clock::now();
-	std::cout << "Time: " << duration_cast<microseconds>(stop - start).count() << std::endl;
+        return (center + (dir * size2));
+    }
 
-	return 0;
+    if constexpr (std::is_same<Shape, Tetrahedron>::value) {
+        vec3 center((s.points[0] + s.points[1] + s.points[2] + s.points[3]) / 4);
+
+        float R = (center - s.points[0]).magnitude();
+
+        vec3 one(1, 1, 1);
+        float sum = (v.x * one.x + v.y * one.y + v.z * one.z);
+        float cosA = (sum / (v.magnitude() * one.magnitude()));
+
+        //unfinished
+        float size = R * cosA;
+
+        return (center + (dir * size));
+    }
+
+    if constexpr (std::is_same<Shape, Capsule>::value) {
+
+        //TO;DO
+        return vec3(1, 1, 1);
+    }
+
+    std::cout << "Not suported yet, return Null vector" << std::endl;
+    return vec3(NULL, NULL, NULL);
+}
+
+int main() {
+
+    vec3 vector{ -2, 5, 3 };
+
+    Sphere sphere{ vec3{0,0,0}, 50 };
+    std::cout << support<Sphere>(vector, sphere) << std::endl;
+
+    Aabb aabb{ vec3{0,0,0}, vec3{100, 100, 100} };
+    std::cout << support<Aabb>(vector, aabb) << std::endl;
+
+    Tetrahedron tetrahedron{ vec3{1,1,1}, vec3{-1,-1,1}, vec3{-1,1,-1}, vec3{1,-1,-1} };
+    std::cout << support<Tetrahedron>(vector, tetrahedron) << std::endl;
+
+    Capsule capsule{ vec3{1,1,1}, vec3{100,100,100}, 50 };
+    std::cout << support<Capsule>(vector, capsule) << std::endl;
+
+    return 0;
 }
